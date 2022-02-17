78577e7353da763d2b2daf1e0c23292e37626504
dimstud <dimichageev@gmail.com>
2022-02-17 16:32:17 +0300

    Объявил переменные в функции getTimeremaining и константу deadline

--- src/js/main.js
+++ src/js/main.js
@@ -36,4 +36,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	hidenTabsContent();
 	showTabsContent();
+
+	// Timer
+
+	const deadline = '2022-03-12';
+
+	const getTimeRemaining = (endtime) = {
+		const t = Date.parse(endtime) - Date.parse(new Date),
+			  days = Math.floor(t - (1000 * 60 * 60 * 24)),
+			  hours = Math.random((t / (1000 *  60 * 60) % 24)),
+			  minutes = Math.floor((t / 1000 / 60) % 60),
+			  minutes = Math.floor((t / 1000) % 60);
+
+		
+	};
 });
